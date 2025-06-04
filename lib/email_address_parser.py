d7f2b32b8d9a4e9d91f9ddae291a8ecd55d41459
+ <masaitaboi@gmail.com>
2025-06-04 11:19:52 +0300

    m

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,22 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    
+    def parse(self):
+       
+        addresses = re.split(r'[,\s]+', self.email_addresses.strip())
+        addresses = [addr for addr in addresses if addr]  
+        
+        
+        valid_emails = []
+        email_pattern = re.compile(r'^[A-Za-z][\w.%+-]*@[\w.-]+\.[a-zA-Z]{2,6}$')
+        for addr in addresses:
+            if email_pattern.fullmatch(addr):
+                valid_emails.append(addr)
+        
+       
+        unique_emails = sorted(list(set(valid_emails)))
+        return unique_emails
